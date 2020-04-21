a0f18e6b81b8e499c8ea40cd09f07b0d3781a0e1
asiwajudev <olanrewajuranda@gmail.com>
2020-04-21 20:49:45 +0100

    Skeleton Ui screen completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+import dummyData from "./Data/Data";
+import CardList from "./Components/Card/CardList";
+import SkeletonCard from "./Components/Skeleton/SkeletonCard";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timer = setTimeout(() => {
+      setVideos(dummyData);
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <SkeletonCard />}
+      {!loading &&
+        videos.map((list, index) => {
+          return (
+            <section key={index}>
+              <h2 className="section-title">{list.section}</h2>
+              <CardList list={list} />
+              <hr />
+            </section>
+          );
+        })}
     </div>
   );
-}
+};
 
 export default App;
